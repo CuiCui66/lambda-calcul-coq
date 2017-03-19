02e2c58f0275be32814f24712aeea6bbdd236dbe
lucas8 <luc.linux@mailoo.org>
2017-03-19 16:14:40 +0100

    Add map function over lamba terms

--- test.v
+++ test.v
@@ -7,3 +7,12 @@ Inductive lterm : Set :=
 Inductive ltype : Set :=
 | base : nat -> ltype
 | fle : ltype -> ltype -> ltype.
+
+Fixpoint term_map (f : nat -> nat) (t : lterm) : lterm :=
+match t with
+| var n     => var (f n)
+| app t1 t2 => app (term_map f t1) (term_map f t2)
+| abs t     => abs (term_map f t)
+end.
+
+
