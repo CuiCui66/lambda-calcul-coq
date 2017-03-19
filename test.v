ed28b49f2f797690f91c6fe5f09ef6bef23e36f8
Thibaut Perami <perami@clipper.ens.fr>
2017-03-19 17:44:10 +0100

    Substi OK

--- test.v
+++ test.v
@@ -17,14 +17,24 @@ end.
 
 Require Import Arith.
 
-Print beq_nat.
+Print leb.
+Fixpoint offset (t : lterm) (decal :nat) (limit : nat) : lterm :=
+  match t with
+  | var i => if leb i limit then var i else var (i+decal)
+  | app t1 t2 => app (offset t1 decal limit) (offset t2 decal limit)
+  | abs t0 => abs (offset t0 decal (S limit))
+  end. 
+
+
 
 Fixpoint substi (t :lterm) (n: nat) (sub : lterm) : lterm :=
   match t with
-  | var m => if beq_nat n m then term_map (fun x => x+n) sub else var m 
+  | var m => if beq_nat n m then offset sub n 0 else var m 
   | app t1 t2 => app (substi t1 n sub) (substi t2 n sub)
   | abs t0 => abs (substi t0 (S n) sub)
   end.
 
+Definition identity := abs (var 0).
 
+Example ident : substi (var 0) 0 identity = identity
 
